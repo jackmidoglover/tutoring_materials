c5da3bf40f8294d241da4c987336dac1bde16787
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-09-20 18:27:30 -0600

    trivia game file structure built, index made

---
+++ jsFundamentals/javascript.js
@@ -0,0 +1,34 @@
+/* Values/ DataTypes
+ We can think of these as the "nouns" of javaScript 
+ they are bits of data that store information and can be 
+ categorized into 4 different types of information*/
+    
+
+ 
+// Operators
+
+
+
+// Expressions vs. Statements
+
+
+
+// Variables also known as "bindings"
+
+
+
+// Functions
+
+
+/* Data Structures
+    These are similar to DataTypes in that they are part of the fundamental building blocks of
+    our code, but they are composed of those different building blocks. An array is a list of 
+    data types. We can think of it as flat- it lists references to other datatypes. An Object is
+    a container of data types that uses key-value pairs. We can think of it as three dimensional.
+    An array is like a shopping list, an object is like your shopping cart. */
+
+// Control flow
+
+// Scope
+
+
